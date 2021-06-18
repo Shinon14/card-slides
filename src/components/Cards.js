0e19a09354288478c0ac031d96f34a3b1fb38f9d
Shion <matixubkn.801@gmail.com>
2021-06-18 00:45:52 -0400

    gh-pages added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,27 +1,26 @@
 import React from "react";
 import Card from "./Card";
-import image1 from '../assets/Blackjack.png'
+import image1 from '../assets/image1.jpg'
 import image2 from '../assets/image2.jpg'
 import image3 from '../assets/image3.jpg'
 
 const cards = [
     {
         id: 1,
-        title: 'BlackJack',
+        title: 'Card-slides',
         image: image1,
-        url: 'https://shinon14.github.io/blackjack/',
-        text: 'Este es un ejemplo de mi primera App web creada unicamente con JavaScript, en el que se destaca el uso de constantes, funciones tipo flecha, carga de imagenes, operadores tenarios y entre muchos, ademas de destacar el Clean Code.'
+        url: ''
     },
     {
         id: 2,
-        title: 'Cards-slides',
+        title: 'Card-slides',
         image: image2,
         url:'https://faztweb.com'
 
     },
     {
         id: 3,
-        title: 'Proximamente...',
+        title: 'Card-slides',
         image: image3,
         url:'https://faztweb.com'
 
